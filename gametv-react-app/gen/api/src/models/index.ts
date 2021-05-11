4e92ef32d1cc02f109a7c7f7c488769e6c0dfb51
milana1211 <wiw.artt@gmail.com>
2021-05-12 02:23:26 +0300

    add schedule and statistics react components

--- gametv-react-app/gen/api/src/models/index.ts
+++ gametv-react-app/gen/api/src/models/index.ts
@@ -1,5 +1,7 @@
 export * from './ErrorResponse';
 export * from './Matches';
 export * from './News';
+export * from './Player1';
+export * from './Player2';
 export * from './Results';
 export * from './Top';
