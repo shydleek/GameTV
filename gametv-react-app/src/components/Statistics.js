4e87e110eada7ee7c2c8afde5a10028cdea880fc
Milana Antonova <79765503+yslsml@users.noreply.github.com>
2021-05-15 10:13:02 +0300

    feat: add layout for Statistics component

--- gametv-react-app/src/components/Statistics.js
+++ gametv-react-app/src/components/Statistics.js
@@ -25,19 +25,44 @@ class Statistics extends React.Component {
 
 
   render() {
-    return <div>
-      <h2>You're on matches(statistics)</h2>
-      {this.state.events.map(
-          (event) =>
-          <div>
-            <h3>{event.team1}</h3>
-            <p><span>{event.players1[0].name} : {event.players1[0].score}</span></p>
-            <p><span>{event.players1[1].name} : {event.players1[1].score}</span></p>
-            <p><span>{event.players1[2].name} : {event.players1[2].score}</span></p>
-            <p><span>{event.players1[3].name} : {event.players1[3].score}</span></p>
-            <p><span>{event.players1[4].name} : {event.players1[4].score}</span></p>
-          </div>
-      )}
+   return <div>
+      <section className="main">
+      <div className="container">
+        <div className="main__inner">
+        <div className="main__stats">
+        <div className="main__stream">
+        </div>
+        {this.state.events.map(
+            (event) =>
+            <div>
+              <div className="main__statistics">
+              <div className="main__statistics__team1">
+              <h3 className="main__schedule__title-link" key={event.id}>{event.team1}:</h3>
+              <div className="players">
+              <p className="player">{event.players1[0].name} : {event.players1[0].score}</p>
+              <p className="player">{event.players1[1].name} : {event.players1[1].score}</p>
+              <p className="player">{event.players1[2].name} : {event.players1[2].score}</p>
+              <p className="player">{event.players1[3].name} : {event.players1[3].score}</p>
+              <p className="player">{event.players1[4].name} : {event.players1[4].score}</p>
+              </div>
+            </div>
+            <div className="main__statistics__team2">
+              <h3 className="main__schedule__title-link">{event.team2}:</h3>
+              <div className="players">
+              <p className="player">{event.players2[0].name} : {event.players2[0].score}</p>
+              <p className="player">{event.players2[1].name} : {event.players2[1].score}</p>
+              <p className="player">{event.players2[2].name} : {event.players2[2].score}</p>
+              <p className="player">{event.players2[3].name} : {event.players2[3].score}</p>
+              <p className="player">{event.players2[4].name} : {event.players2[4].score}</p>
+              </div>
+            </div>
+            </div>
+            </div>
+        )}
+      </div>
+      </div>
+      </div>
+      </section>
     </div>
   }
 }
